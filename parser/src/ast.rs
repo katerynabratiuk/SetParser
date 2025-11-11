ca83d1043ad917de9e859f79eba4281b9ae58471
Kateryna Bratiuk <kate.bratiuk@gmail.com>
2025-11-11 15:09:55 +0200

    Define syntax tree

---
+++ parser/src/ast.rs
@@ -0,0 +1,23 @@
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Expr {
+    Ident(String),
+    Empty,
+    SetLiteral(Vec<i32>),              // {1,2,3}
+    Range(i32, i32),                   // {a..b} inclusive
+    Complement(Box<Expr>),             // A'
+    Union(Box<Expr>, Box<Expr>),       // A ∪ B
+    Intersect(Box<Expr>, Box<Expr>),   // A ∩ B
+    Diff(Box<Expr>, Box<Expr>),        // A \ B
+    SymDiff(Box<Expr>, Box<Expr>),     // A △ B
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Stmt {
+    Let { name: String, value: Expr },
+    Print(Expr),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Program {
+    pub stmts: Vec<Stmt>,
+}
